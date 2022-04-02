56705c89780f19f17d41aab7a5e14e2ffa0162b4
chromatic-wolf <34954828+chromatic-wolf@users.noreply.github.com>
2022-04-02 18:51:30 +1000

    Fixed downloader
    Fixed download so it now works

--- src/downloader.rs
+++ src/downloader.rs
@@ -1,28 +1,17 @@
-use std::{fs::File, io, collections::HashMap};
+use std::{fs::File, io};
 
 pub fn download_file(path: String, url: String)
 {
     
-        let do_steps = || -> Result<(), Box<dyn std::error::Error>> {
-            let resp = reqwest::blocking::get(url)?
-                .json::<HashMap<String, String>>()?;
-
-            //println!("{:#?}", resp);
-
-            for (key, value) in &resp {
-                if key == "message" {
+    
                     //print!("{}", value);
-                    let mut resp = reqwest::blocking::get(value).expect("request failed");
+                    let mut resp = reqwest::blocking::get(url).expect("request failed");
                     let mut out = File::create(path.clone()).expect("failed to create file");
                     io::copy(&mut resp, &mut out).expect("failed to copy content");
-                }
-            }
-
-            Ok(())
-        };
+                
         
-        if let Err(_err) = do_steps() {
-            eprintln!("Application error: {}", _err);
-        }
+       
+        
+      
 
     }
