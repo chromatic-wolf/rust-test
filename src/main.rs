1a4dc6bc9f5856b4c0da014c4c37d3463669121c
chromatic-wolf <34954828+chromatic-wolf@users.noreply.github.com>
2022-04-07 15:48:10 +1000

    Added html file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 mod downloader;
 mod serialiser;
 use std::time::Instant;
-fn main() -> std::io::Result<()> {
+use yew::prelude::*;
+
+fn download() -> std::io::Result<()> {
     let now = Instant::now();
     downloader::download_file(
         String::from("test.zip"),
@@ -11,4 +13,49 @@ fn main() -> std::io::Result<()> {
     println!("Elapsed: {:.2?}", elapsed);
 
     Ok(())
+}
+
+enum Msg {
+    AddOne,
+}
+
+struct Model {
+    value: i64,
+}
+
+impl Component for Model {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self {
+            value: 0,
+        }
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::AddOne => {
+                self.value += 1;
+                // the value has changed so we need to
+                // re-render for it to appear on the page
+                true
+            }
+        }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        // This gives us a component's "`Scope`" which allows us to send messages, etc to the component.
+        let link = ctx.link();
+        html! {
+            <div>
+                <button onclick={link.callback(|_| Msg::AddOne)}>{ "+1" }</button>
+                <p>{ self.value }</p>
+            </div>
+        }
+    }
+}
+
+fn main() {
+    yew::start_app::<Model>();
 }
